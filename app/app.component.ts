9d39a735edc8f0f04fbb30458285044cdcd78f81
DarriusWrightGD <darriuswrightgd@gmail.com>
2016-06-12 22:25:54 -0600

    Completed barcode demo

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,13 +8,13 @@ var Toast = require("nativescript-toast");
     template: `
 <StackLayout>
     <Label text="Click to scan" class="title"></Label>
-    <Button text="Scan" (tap)="scan()"></Button>
-    <Label [text]="message" class="message" textWrap="true"></Label>
+    <Button text="Scan Part" (tap)="scan()"></Button>
+    <Label [text]="message" [hidden]="message == ''" class="message" textWrap="true"></Label>
 </StackLayout>
 `,
 })
 export class AppComponent implements OnInit {
-    scanResult = ""; 
+    scanResult = ''; 
     
     ngOnInit(){
         barcodescanner.available().then(()=>{
@@ -23,9 +23,11 @@ export class AppComponent implements OnInit {
                             console.log("Permission to use the camera: " + granted);
                         }
                         else{
-                            barcodescanner.requestCameraPermission().then(()=>{
-                                console.log("Requesting permission to use the camera")
+                            var obtainedResult = barcodescanner.requestCameraPermission().then(()=>{
+                                console.log("Requesting permission to use the camera");
                             });
+                            
+                            obtainedResult.then((response)=>console.log("Response: " + JSON.stringify(response)));
                         }
                     }
                 );
@@ -35,16 +37,20 @@ export class AppComponent implements OnInit {
     
     public scan(){
         console.log("scanning")
-        barcodescanner.scan({
+        var result = barcodescanner.scan({
             formats: "AZTEC,UPC_E,EAN_13,EAN_8,CODE_128,CODE_93,CODE_39,QR_CODE,PDF_417",
             cancelLabel: "Stop scanning", // iOS only, default 'Close' 
-            message: "Scan the part", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.' 
+            message: "Scan for parts", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.' 
             orientation: "landscape"      // Android only, optionally lock the orientation to either "portrait" or "landscape" 
         })
         .then((result)=> this.scanResult = result, (error)=> this.scanResult = error);
+        result.then((r)=> console.log("barcode result: " + JSON.stringify(r)));
+       
     }
 
     public get message(): string {
+        if(this.scanResult == '')
+            return this.scanResult
         return JSON.stringify(this.scanResult);
     }
 }
